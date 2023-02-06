8e66fd7972334105aacc1aa414cbf117da1ef729
ziopeo <15086554+ziopeo@users.noreply.github.com>
2023-02-06 19:35:17 +0100

    Componente per il form di registrazione utente

---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,32 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState, AppThunk } from "../../app/store";
+interface UserData {
+  username: string;
+  email: string;
+}
+
+interface UpdateUserDataPayload {
+  username: string;
+  email: string;
+}
+
+const initialState: UserData = {
+  username: "",
+  email: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateUserData: (state, action: PayloadAction<UpdateUserDataPayload>) => {
+      const { username, email } = action.payload;
+      state.username = username;
+      state.email = email;
+    },
+  },
+});
+export const select = (state: RootState) => state.user;
+export const { updateUserData } = userSlice.actions;
+
+export default userSlice.reducer;
