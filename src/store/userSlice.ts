4f9359670dd179cd19a7c6b50be85f44b6bb349d
ziopeo <15086554+ziopeo@users.noreply.github.com>
2023-02-08 01:08:25 +0100

    Fix UserData interface

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,32 +1,33 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { RootState, AppThunk } from "./store";
-interface UserData {
-  username: string;
-  email: string;
-}
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./store";
+import {UserData } from "../services/interfaces"
 
-interface UpdateUserDataPayload {
-  username: string;
-  email: string;
-}
 
+// Stato iniziale dei dati dell'utente
 const initialState: UserData = {
   username: "",
   email: "",
 };
 
+// Creazione del slice per i dati dell'utente
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    updateUserData: (state, action: PayloadAction<UpdateUserDataPayload>) => {
+    // Reducer per l'aggiornamento dei dati dell'utente
+    updateUserData: (state, action: PayloadAction<UserData>) => {
       const { username, email } = action.payload;
       state.username = username;
       state.email = email;
     },
   },
 });
+
+// Seleziona i dati dell'utente dallo stato globale
 export const select = (state: RootState) => state.user;
+
+// Azioni per l'aggiornamento dei dati dell'utente
 export const { updateUserData } = userSlice.actions;
 
+// Reducer per i dati dell'utente
 export default userSlice.reducer;
